b4abe742f2beb6aa0315464bb12c25aeedfc2ea4
Alec <amsears60441@gmail.com>
2022-05-08 13:00:17 -0600

    git cloned from solution repo

---
+++ 8_github_auth/server/main.js
@@ -0,0 +1,48 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { ServiceConfiguration } from 'meteor/service-configuration';
+
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task',
+    ].forEach(taskText => insertTask(taskText, user));
+  }
+});
+
+ServiceConfiguration.configurations.upsert(
+  { service: 'github' },
+  {
+    $set: {
+      loginStyle: 'popup',
+      clientId: 'b76ccea950d927ab7429', // insert your clientId here
+      secret: '1a5988ee93bd6dcc06d71c95c5f14ae592da55fc', // insert your secret here
+    },
+  }
+);
